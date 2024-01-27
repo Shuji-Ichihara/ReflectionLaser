67ebfb46139d61721837409da707177efcd0cb4e
aoshika02 <135197276+aoshika02@users.noreply.github.com>
2024-01-27 17:50:40 +0900

    マップとゴール完成

---
+++ ReflectionLaser/Assets/Member/Yuta/Scripts/Test_GameManger.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_GameManger : MonoBehaviour
+{
+    public void Player_Goal()
+    {
+        Debug.Log("Gole");
+    }
+}
