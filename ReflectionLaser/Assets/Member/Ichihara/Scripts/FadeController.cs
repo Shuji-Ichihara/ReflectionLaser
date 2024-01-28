92800456277fc2dfe3cfc7539a580ced42943a16
Shuji-Ichihara <shuji.birth-0507@outlook.jp>
2024-01-28 12:06:58 +0900

    Update FadeController.cs
    シーンの読み込み時にフェード用キャンバスを生成する delegate を追記した。

--- ReflectionLaser/Assets/Member/Ichihara/Scripts/FadeController.cs
+++ ReflectionLaser/Assets/Member/Ichihara/Scripts/FadeController.cs
@@ -1,46 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+using Cysharp.Threading.Tasks;
+using System.Threading;
 
 public class FadeController : SingletonMonoBehaviour<FadeController>
 {
+    // フェード処理に使用するキャンバス
     [SerializeField]
     private Canvas _fadeCanvas = null;
-
+    // フェード処理に使用するマテリアル
     [SerializeField]
     private Material _fadeMaterial = null;
+    // フェード処理に掛ける時間
     [SerializeField]
     private float _defaultFadeTime = 2f;
+    // フェード処理用キャンバスのタグ名
+    private readonly string _canvasTagName = "Fade";
 
     protected override void Awake()
     {
         base.Awake();
         DontDestroyOnLoad(this);
-        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     /// <summary>
     /// FadeInTransition を呼び出す関数
     /// </summary>
     /// <param name="fadeTime">フェード処理に掛ける時間</param>
-    public void CallFadeInTransition(float fadeTime = 0f)
+    public async UniTask CallFadeInTransition(float fadeTime = 0f, CancellationTokenSource cts = default)
     {
         // 三項演算子を用いて、外部からフェードの秒数を指定できるようにした
         fadeTime = fadeTime <= 0f ? _defaultFadeTime : fadeTime;
-        StartCoroutine(FadeInTransition(_fadeMaterial, fadeTime));
+        await FadeInTransition(_fadeMaterial, fadeTime, cts);
     }
 
     /// <summary>
     /// FadeOutTransition を呼び出す関数
     /// </summary>
     /// <param name="fadeTime">フェード処理に掛ける時間</param>
-    public void CallFadeOutTransition(float fadeTime = 0f)
+    private async UniTask CallFadeOutTransition(float fadeTime = 0f, CancellationTokenSource cts = default)
     {
         // 三項演算子を用いて、外部からフェードの秒数を指定できるようにした
         fadeTime = fadeTime <= 0f ? _defaultFadeTime : fadeTime;
-        StartCoroutine(FadeOutTransition(_fadeMaterial, fadeTime));
+        await FadeOutTransition(_fadeMaterial, fadeTime, cts);
     }
 
     /// <summary>
@@ -49,15 +51,17 @@ public class FadeController : SingletonMonoBehaviour<FadeController>
     /// <param name="material">ルール画像を適用したマテリアル</param>
     /// <param name="fadeTime">フェード処理にかける秒数</param>
     /// <returns></returns>
-    private IEnumerator FadeInTransition(Material material, float fadeTime)
+    private async UniTask FadeInTransition(Material material, float fadeTime, CancellationTokenSource cts = default)
     {
         var image = _fadeCanvas.GetComponentInChildren<Image>();
         image.material = material;
         float currentTime = 0f;
         while (currentTime < fadeTime)
         {
+            // キャンセルされたら、強制的にフェードインを実行する
+            if (cts.IsCancellationRequested == true) break;
             material.SetFloat("_Alpha", currentTime / fadeTime);
-            yield return null;
+            await UniTask.Yield();
             currentTime += Time.deltaTime;
         }
         material.SetFloat("_Alpha", 1f);
@@ -69,22 +73,35 @@ public class FadeController : SingletonMonoBehaviour<FadeController>
     /// <param name="material">ルール画像を適用したマテリアル</param>
     /// <param name="fadeTime">フェード処理にかける秒数</param>
     /// <returns></returns>
-    private IEnumerator FadeOutTransition(Material material, float fadeTime)
+    private async UniTask FadeOutTransition(Material material, float fadeTime, CancellationTokenSource cts = default)
     {
         var image = _fadeCanvas.GetComponentInChildren<Image>();
         image.material = material;
         float currentTime = 0f;
         while (currentTime < fadeTime)
         {
+            // キャンセルされたら、強制的にフェードアウトを実行する
+            if (cts.IsCancellationRequested == true) break;
             material.SetFloat("_Alpha", 1 - currentTime / fadeTime);
-            yield return null;
+            await UniTask.Yield();
             currentTime += Time.deltaTime;
         }
         material.SetFloat("_Alpha", 0f);
     }
 
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    /// <summary>
+    /// シーンの読み込み時に、フェード処理用のキャンバスを生成する
+    /// </summary>
+    /// <param name="scene"></param>
+    /// <param name="mode"></param>
+    public void InstantiateCanvas(UnityEngine.SceneManagement.Scene scene,
+                                  UnityEngine.SceneManagement.LoadSceneMode mode)
     {
+        var canvas = GameObject.FindGameObjectWithTag(_canvasTagName);
+        if (canvas != null)
+            Destroy(canvas);
+        var cts = new CancellationTokenSource();
         Instantiate(_fadeCanvas.gameObject);
+        CallFadeOutTransition(cts: cts).Forget();
     }
 }
